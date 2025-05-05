0acea93c0da8df2230c78ef7b06930a5a62a106e
Arbaz158 <arbazakhtar389@gmail.com>
2025-05-05 22:43:21 +0530

    Add generated tests for PR 1

---
+++ test_generated/pr_1_tests_20250505_224320.py
@@ -0,0 +1,21 @@
+# Generated tests for PR 1
+# Generated on: 2025-05-05 22:43:20
+
+
+# Test: test_generated
+The generated tests are as follows:
+
+For the file "math/operations_test.go":
+
+- TestAdd: Tests the Add function with inputs 3 and 5, expecting a result of 8.
+- TestSubtract: Tests the Subtract function with inputs 10 and 5, expecting a result of 5.
+- TestMultiply: Tests the Multiply function with inputs 4 and 6, expecting a result of 24.
+- TestDivide: Tests the Divide function with inputs 10 and 2, expecting a result of 5 and no error.
+- TestDivideByZero: Tests the Divide function with inputs 10 and 0, expecting an error.
+- TestPower: Tests the Power function with inputs 2 and 3, expecting a result of 8.
+- TestSquareRoot: Tests the SquareRoot function with input 16, expecting a result of 4 and no error.
+- TestSquareRootNegativeNumber: Tests the SquareRoot function with input -16, expecting an error.
+- TestSquareRootZeroAndOne: Tests the SquareRoot function with inputs 0 and 1, expecting results of 0 and 1 respectively and no errors.
+- TestAbs: Tests the Abs function with input -5, expecting a result of 5.
+
+These tests are provided in both Go and Python formats.
