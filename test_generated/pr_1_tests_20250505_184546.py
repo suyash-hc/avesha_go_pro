5f21c21138cdf8a88bccfd5db42fe02a11fd1492
Arbaz158 <arbazakhtar389@gmail.com>
2025-05-05 22:29:33 +0530

    removed test case file

--- test_generated/pr_1_tests_20250505_184546.py
+++
@@ -1,6 +0,0 @@
-# Generated tests for PR 1
-# Generated on: 2025-05-05 18:45:46
-
-
-# Test: test_generated
-It's not possible to generate tests directly from the provided git diff using the available tools. The generate_tests function requires a git diff in JSON format as input.
