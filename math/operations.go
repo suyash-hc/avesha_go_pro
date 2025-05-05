a55abeb5a717b639b9d4366c734cdda926ab6c7a
Arbaz158 <arbazakhtar389@gmail.com>
2025-05-05 15:00:19 +0530

    added more functionality

--- math/operations.go
+++ math/operations.go
@@ -1,5 +1,7 @@
 package math
 
+import "errors"
+
 // Add returns the sum of two integers
 func Add(a, b int) int {
 	return a + b
@@ -8,4 +10,17 @@ func Add(a, b int) int {
 // Subtract returns the difference between two integers
 func Subtract(a, b int) int {
 	return a - b
+}
+
+// Multiply returns the product of two integers
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide returns the quotient of two integers
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
 }
