f178a30559fdba0101fae29386c1ce5867926fee
suyash-hc <suyash.nikam@humancloud.co.in>
2025-05-07 12:45:09 +0530

    Add generated tests for PR 5

--- test_generated/pr_5_tests_20250507_120404_test.go
+++ test_generated/pr_5_tests_20250507_120404_test.go
@@ -6,18 +6,10 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-\n\tcases := []testCase{\n\t\t{2, 3, 5, false
+\n\tcases := []struct {\n\t\ta, b, expected int\n\t
 }
 
 func TestSubtract(t *testing.T) {
-\n\tcases := []testCase{\n\t\t{5, 3, 2, false
-}
-
-func TestMultiply(t *testing.T) {
-\n\tcases := []testCase{\n\t\t{2, 3, 6, false
-}
-
-func TestDivide(t *testing.T) {
-\n\tcases := []struct {\n\t\ta, b int\n\t\texpected int\n\t\terrorExpected bool\n\t
+\n\tcases := []struct {\n\t\ta, b, expected int\n\t
 }
 
