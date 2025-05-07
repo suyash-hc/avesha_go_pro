e9ffa4f18e393eea348bc34d7093845f20a21ccb
suyash-hc <suyash.nikam@humancloud.co.in>
2025-05-07 11:48:51 +0530

    Add generated tests for PR 3

---
+++ test_generated/pr_3_tests_20250507_114851_test.go
@@ -0,0 +1,96 @@
+package test_generated
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
+func TestSubtract(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
+func TestMultiply(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
+func TestDivide(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		a             int
+		b             int
+		expectedValue int
+		expectedError bool
+	
+}
+
+func TestPower(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
+func TestSquareRoot(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		a             int
+		expectedValue int
+		expectedError bool
+	
+}
+
+func TestAbs(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		expected int
+	
+}
+
+func TestMax(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
+func TestMin(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	
+}
+
