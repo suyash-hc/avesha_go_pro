70da24e23e398b1382c2547280ccf4e8d4a3458b
suyash-hc <suyash.nikam@humancloud.co.in>
2025-05-07 12:40:03 +0530

    Add generated tests for PR 5

--- test_generated/pr_5_tests_20250507_120404_test.go
+++ test_generated/pr_5_tests_20250507_120404_test.go
@@ -2,5 +2,22 @@ package test_generated
 
 import (
 	"testing"
+	\n\t"testing"\n\t"test-avesha-agent/math"\n
 )
+
+func TestAdd(t *testing.T) {
+\n\tcases := []testCase{\n\t\t{2, 3, 5, false
+}
+
+func TestSubtract(t *testing.T) {
+\n\tcases := []testCase{\n\t\t{5, 3, 2, false
+}
+
+func TestMultiply(t *testing.T) {
+\n\tcases := []testCase{\n\t\t{2, 3, 6, false
+}
+
+func TestDivide(t *testing.T) {
+\n\tcases := []struct {\n\t\ta, b int\n\t\texpected int\n\t\terrorExpected bool\n\t
+}
 
